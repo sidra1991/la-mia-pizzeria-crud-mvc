79520117af929c379cb53a7deb432bc6a2d5775d
anthony cecconi <anthony.cecconi.91@gmail.com>
2022-11-22 15:43:10 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,52 @@
+﻿using la_mia_pizzeria_static.data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            PizzaDB pi = new PizzaDB();
+
+            List<Pizza> pizzaList = pi.pizze.ToList();
+
+            return View("Index", pizzaList);
+        }
+
+        public IActionResult Show(int id)
+        {
+            PizzaDB pi = new PizzaDB();
+
+            Pizza pizza = pi.pizze.Where(p => p.Id == id).FirstOrDefault();
+
+            return View(pizza);
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaDB pi = new PizzaDB();
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+
+            pi.pizze.Add(pizza);
+            pi.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,37 @@
+﻿using la_mia_pizzeria_static.data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Models
+{
+
+
+    public class Pizza
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Url (ErrorMessage = "questo deve essere un url valido") ]
+        public string ImageAddress { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(100, ErrorMessage = "la descrizione è troppo lunga")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Range(1, 10, ErrorMessage = "il prezzo non è valido")]
+        public int Price { get; set; }
+        public Pizza()
+        {
+
+        }
+        public Pizza(string name, string imageAddress, string description, int price)
+        {
+            Name = name;
+            ImageAddress = imageAddress;
+            Description = description;
+            Price = price;
+        }
+
+    }
+}
